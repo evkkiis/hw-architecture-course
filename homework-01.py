e08343b7904ce9cc9a2bafd65f8e1f8c424eab9c
evkkiis <evebulaeva@yandex.ru>
2026-09-16 21:06:30 +0300

    создала папку для домашнего задания

---
+++ homework-01.py
@@ -0,0 +1,28 @@
+# Задание №1.
+# ¬(A ∧ B) ∨ ¬(A ∨ C)
+print ("A B C f")
+for A in [0,1]:
+    for B in [0,1]:
+        for C in [0,1]:
+            f = not (A and B) or not (A or C)
+            print (A, B, C, int(f))
+
+
+# Задание №2.
+# (A ∧ B) ∨ (¬B ∧ C)
+print ("A B C f")
+for A in [0,1]:
+    for B in [0,1]:
+        for C in [0,1]:
+            f = (A and B) or (not B and C)
+            print(A, B, C, int(f))
+
+
+# Задание №3.
+# (A ∧ B) ∨ ¬C
+print("A B C f")
+for A in [0,1]:
+    for B in [0,1]:
+        for C in [0,1]:
+            f = (A and B) or not C
+            print(A, B, C, int(f))
